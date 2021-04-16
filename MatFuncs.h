13e039139aeb559415c35b6311b1e1233e3a29d8
CaitaXD <caitaXD@gmail.com>
2021-04-16 20:38:45 -0300

    Implementado Funçoes de mover os numeros da matris

--- MatFuncs.h
+++ MatFuncs.h
@@ -1,36 +1,213 @@
 #pragma once
-#define MAX 500
+#define MAX 10
 
 int FillMat(int mat[][MAX], int l, int c);
 int InputA(int mat[][MAX], int l, int c);
 
-int FillMat(int mat[][MAX], int l, int c)
+int InitMat(int mat[][MAX], int l, int c)
 {
-
+    int count = 2;
     system("cls");
     for (int x = 0; x < l; x++)
     {
         for (int y = 0; y < c; y++)
         {
-            *(mat[0] + x * MAX + y) = rand() % 4;
+            int  sort = rand() % 100;
+            if (sort < 90 && count > 0)
+            {
+                count--;
+                mat[x][y] = 2;
+            }
+            else if (sort >= 90 && count > 0)
+            {
+                count--;
+                mat[x][y] = 4;
+            }
+            else
+                mat[x][y] = 0;
         }
     }
+    return mat;
+}
+int FillMat(int mat[][MAX], int l, int c)
+{
+    int count = 2;
+    system("cls");
+    for (int x = 0; x < l; x++)
+    {
+        for (int y = 0; y < c; y++)
+        {
+            int  sort = rand() % 100;
+            if (sort < 90 && count > 0 && mat[x][y] == 0)
+            {
+                count--;
+                mat[x][y] = 2;
+            }
+            else if (sort > 90 && count > 0 && mat[x][y] == 0)
+            {
+                count--;
+                mat[x][y] = 2;
+            }
+        }
+    }
+    return mat;
+}
+int InputD(int mat[][MAX], int l, int c)
+{
+    //Slide the matrix
+    int k, j, aux;
+    for (int h = 0; h < l; h++)
+    {
+        for (k = l - 1; k > 0; k--)
+        {
+
+            for (j = 0; j < k; j++)
+            {
+
+                if (mat[h][j + 1] == 0) {
+                    aux = mat[h][j];
+                    mat[h][j] = mat[h][j + 1];
+                    mat[h][j + 1] = aux;
+                }
+                if (mat[h][j] == mat[h][j + 1])
+                { 
+                    aux = 2*mat[h][j];
+                    mat[h][j] =0;
+                    mat[h][j + 1] = aux;
+                }
+            }
+        }
+    }
+    FillMat(mat, l, c);
     return mat;
 }
 int InputA(int mat[][MAX], int l, int c)
 {
-    for (int x = 0; x < l; x++)
+    int aux = 1;
+    // Double The Equal Values
+    for (int h = 0; h < l; h++)
     {
-        for (int y = 0; y-1 < c; y++)
+        for (int k = l - 1; k > 0; k--)
         {
-            if (mat[x][y+1] == mat[x][y])
+            for (int j = 0; j < k; j++)
             {
-                *(mat[0] + x * MAX + y) = 2*mat[x][y];
-                *(mat[0] + x * MAX + (y + 1)) = 0;
+                if (mat[h][j] == mat[h][j + 1])
+                {
+                    aux = 2 * mat[h][j];
+                    mat[h][j] = 0;
+                    mat[h][j + 1] = aux;
+                }
             }
-
         }
     }
+    for (int h = 0; h < l; h++)
+    {
+        for (int k = l - 1; k > 0; k--)
+        {
+          for (int j = 0; j < k; j++)
+            {
+                if (mat[h][j] == 0) 
+                {
+                    aux = mat[h][j];
+                    mat[h][j] = mat[h][j + 1];
+                    mat[h][j + 1] = aux;
+                }
+
+            }
+        }
+    }
+    FillMat(mat, l, c);
     return mat;
 }
 
+int InputW(int mat[][MAX], int l, int c)
+{
+    int aux;
+    for (int h = 0; h < l; h++)
+    {
+        for (int k = l - 1; k > 0; k--)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                if (mat[j + 1][h] == mat[j][h])
+                {
+                    aux = 2 * mat[j][h];
+                    mat[j][h] = 0;
+                    mat[j+1][h] = aux;
+                }
+            }
+        }
+    }
+    for (int h = 0; h < l; h++)
+    {
+        for (int k = l - 1; k > 0; k--)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                if (mat[j][h] == 0)
+                {
+                    aux = mat[j+1][h];
+                    mat[j+1][h] = mat[j][h];
+                    mat[j][h] = aux;
+                }
+            }
+        }
+    }
+    FillMat(mat, l, c);
+    return mat;
+}
+int InputS(int mat[][MAX], int l, int c)
+{
+    int aux;
+
+    //Slide Matrix == Para Baixo
+    for (int h = 0; h < l; h++)
+    {
+        for (int k = l - 1; k > 0; k--)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                if (mat[j + 1][h] == 0)
+                {
+                    aux = mat[j][h];
+                    mat[j][h] = mat[j + 1][h];
+                    mat[j + 1][h] = aux;
+                }
+            }
+        }
+    }
+    //Dobra os Valores Repetido
+    for (int h = 0; h < l; h++)
+    {
+        for (int k = l - 1; k > 0; k--)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                if (mat[j + 1][h] == mat[j][h])
+                {
+                    aux = 2 * mat[j][h];
+                    mat[j + 1][h] = 0;
+                    mat[j][h] = aux;
+                }
+            }
+        }
+    }
+    //Slide Matrix De pq sobra um 0 entre os valors visto que e função le em pares
+    for (int h = 0; h < l; h++)
+    {
+        for (int k = l - 1; k > 0; k--)
+        {
+            for (int j = 0; j < k; j++)
+            {
+                if (mat[j + 1][h] == 0)
+                {
+                    aux = mat[j][h];
+                    mat[j][h] = mat[j + 1][h];
+                    mat[j + 1][h] = aux;
+                }
+            }
+        }
+    }
+    FillMat(mat, l, c);
+    return mat;
+}
--- main.c
+++ main.c
@@ -2,16 +2,28 @@
 #include <stdlib.h>
 #include "MatFuncs.h"
 
+/*
+Nno momento usa-se o scanf como input
+
+Creio que o comportamento da matriz esteje  correto mas e possivel que não
+
+Para mover os numeros na matriz foi adaptado bublesort dentro de um laço for
+
+Algumas direçõe presisaram de mais laços por causa da ordem de varredura 
+
+As funçoes de Input retornam a matriz
+    (n sei se vale a mão de mudar o retorno para void e usar pointer?)
+*/
+
 int EmptyScreen(int mat[][MAX],int l,int c)
 {
-
     system("cls");
     for (int x = 0; x < l; x++)
     {
         for (int y = 0; y < c; y++)
         {
            *(mat[0] + x *MAX + y) = 0;
-            printf("%4d", mat[x][y]);
+            printf("%7d", mat[x][y]);
         }
         printf("\n");
     }
@@ -20,12 +32,12 @@ int EmptyScreen(int mat[][MAX],int l,int c)
 
 int UpdateScreen(int mat[][MAX], int l, int c)
 {
-    system("cls");
+    system("cls");;
     for (int x = 0; x < l; x++)
     {
         for (int y = 0; y < c; y++)
         {
-            printf("%4d", mat[x][y]);
+            printf("%7d", mat[x][y]);
         }
         printf("\n");
     }
@@ -33,32 +45,41 @@ int UpdateScreen(int mat[][MAX], int l, int c)
 }
 char Input()
 {
-    int input;
-    scanf_s(" %c", &input);
-    return input;
+    char c;
+    scanf_s(" %c", &c);
+    return c;
 }
 void HandleInput(int mat[][MAX], int l, int c)
 {
     while (1)
-    {
+    {    
         switch (Input())
         {
         default:
             break;
         case 'a':
-            InputA(*mat, l, c);
+            InputA(mat, l, c);
+            break;
+        case 'd':
+            InputD(mat, l, c);
+            break;
+        case 'w':
+            InputW(mat, l, c);
+            break;
+        case 's':
+            InputS(mat, l, c);
             break;
         }
-     UpdateScreen(*mat, l, c);
+     UpdateScreen(mat, l, c);
     }
 }
 int main()
 {  
     int l = 4, c = 4;
     int mat[MAX][MAX];
-    FillMat(*mat, l, c);
-    UpdateScreen(*mat, l, c);
-    HandleInput(*mat, l, c);
+    InitMat(mat, l, c);
+    UpdateScreen(mat, l, c);
+    HandleInput(mat, l, c);
 
     return 0;
 }
