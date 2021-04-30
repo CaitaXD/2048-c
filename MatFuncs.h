b04474e4b9cda459a9c4be30151311eacad2c80b
marcusbicca <83423983+marcusbicca@users.noreply.github.com>
2021-04-29 21:44:46 -0300

    Update MatFuncs.h

--- MatFuncs.h
+++ MatFuncs.h
@@ -7,10 +7,8 @@
 int FillMat (int mat[][MAX], int l, int c);
 int InitMat (int mat[][MAX], int l, int c);
 int InputW (int mat[][MAX], int l, int c);
-int slideW (int mat[][MAX], int l, int c);
 int InputA (int mat[][MAX], int l, int c);
 int InputS (int mat[][MAX], int l, int c);
-int slideS (int mat[][MAX], int l, int c);
 int InputD (int mat[][MAX], int l, int c);
 int EmptyScreen(int mat[][MAX],int l,int c);
 void HandleInput(int mat[][MAX], int l, int c);
