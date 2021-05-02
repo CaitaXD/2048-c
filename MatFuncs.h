cf49cab00da9c4efe319c00d18b9bfec9784179b
marcusbicca <83423983+marcusbicca@users.noreply.github.com>
2021-05-02 02:49:37 -0300

    Update MatFuncs.h

--- MatFuncs.h
+++ MatFuncs.h
@@ -5,7 +5,7 @@
 #include<time.h>
 #define LIM 10
 #define MAX 4
-int FillMat (int mat[][MAX]);
+int FillMat (int mat[][MAX], int a);
 int InitMat (int mat[][MAX]);
 int InputW (int mat[][MAX]);
 int InputA (int mat[][MAX]);
